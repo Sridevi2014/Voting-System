4dbcda07fb3091f9f8527f0416ebcb8d74114d70
Sridevi Patchipulusu <sridevips@hotmail.com>
2020-01-09 13:36:36 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,26 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {;
+    constructor(props){
+		super(props);
+		this.state = {
+			languages : [
+				{name: "Php", votes: 0},
+				{name: "Python", votes: 0},
+				{name: "Go", votes: 0},
+				{name: "Java", votes: 0}
+			]
+		}
+	}
+
+	
+
+  render() {
+    return (
+       <div></div>
+    )
+}
 }
 
 export default App;
