7478ea723861e5b2d136ca65e8b23139e1abd5db
Sridevi Patchipulusu <sridevips@hotmail.com>
2020-01-09 14:44:52 -0600

    added swap function, onClick Event

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
 import './App.css';
 
-class App extends Component {;
+class App extends Component {
+    
     constructor(props){
 		super(props);
 		this.state = {
@@ -14,13 +15,44 @@ class App extends Component {;
 		}
 	}
 
-	
+	vote (i) {
+		let newLanguages = [...this.state.languages];
+		newLanguages[i].votes++;
+		function swap(array, i, j) {
+			var temp = array[i];
+			array[i] = array[j];
+			array[j] = temp;
+		}
+		this.setState({languages: newLanguages});
+		
+    }
+    
+    
+    render() {
+        return (
+            <>
+                <h1>Vote Your Language!</h1>
 
-  render() {
-    return (
-       <div></div>
-    )
-}
+                <div className="languages">
+                    {
+                        this.state.languages.map((lang, i) =>
+                            <div key={i} className="language">
+                                <div className="voteCount">
+                                    {lang.votes}
+                                </div>
+                                <div className="languageName">
+                                    {lang.name}
+                                </div>
+                                <button onClick={this.vote.bind(this, i)}>Click Here</button>
+                            </div>
+                        )
+                    }
+                </div>
+
+
+            </>
+        )
+    }
 }
 
 export default App;
